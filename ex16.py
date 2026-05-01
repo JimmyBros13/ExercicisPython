82015ca200885b1f57ce64e9161fb41b90a5e748
GMata <guillem.mata13@gmail.com>
2026-05-01 23:42:29 +0200

    Delete ex16.py

--- ex16.py
+++
@@ -1,9 +0,0 @@
-#Exercici 16
-
-patron="54321"
-for i in range(len(patron)):
-    print(patron[i:])
-#for i in range(len(patron),-1,-1):
-#    print(patron[i:])
-#for i in range(len(patron),-1,-1):
-#    print(patron[:i])
